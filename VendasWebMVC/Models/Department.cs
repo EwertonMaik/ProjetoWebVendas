0916e8683667a2b461d55c866d5d3b956524dd04
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-26 11:01:40 -0300

    Adicionado Primeiro Model-Controller-View Department

---
+++ VendasWebMVC/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace VendasWebMVC.Models
+{
+    //classe Entidade Departamentos
+    public class Department
+    {
+        //Atributos AutoProperties
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
