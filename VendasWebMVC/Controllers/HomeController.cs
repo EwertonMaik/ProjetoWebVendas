9f25924717045ab73e95c2f256d7762e5e1d6fbc
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-26 10:07:52 -0300

    Adicionado Alteração no Controller

--- VendasWebMVC/Controllers/HomeController.cs
+++ VendasWebMVC/Controllers/HomeController.cs
@@ -17,8 +17,8 @@ namespace VendasWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
-
+            ViewData["Message"] = "Aplicação Web Vendas";
+            ViewData["Aluno"] = "Ewerton Maik";
             return View();
         }
 
