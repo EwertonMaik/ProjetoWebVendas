868ad265c6bbe69adaef9e34de83a156ee514954
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-26 21:15:24 -0300

    Criado SellerService - Serviço de Vendedores com listagem

--- VendasWebMVC/Controllers/SellersController.cs
+++ VendasWebMVC/Controllers/SellersController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VendasWebMVC.Services;
 
 namespace VendasWebMVC.Controllers
 {
     public class SellersController : Controller
     {
+        //Decladando dependência de SellerService
+        private readonly SellerService _sellerService;
+
+        //Definindo Contrutor para injetar Dependência
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll(); //Retorna um Lista de Vendedores
+            return View(list);
         }
     }
 }
---
+++ VendasWebMVC/Services/SellerService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Data;
+using VendasWebMVC.Models;
+
+namespace VendasWebMVC.Services
+{
+    //Classe Serviços de Vendedor
+    public class SellerService
+    {
+        //Declarando Dependência, com readonly para que não possa ser alterada
+        private readonly VendasWebMVCContext _context;
+
+        //Toda vez que for instânciado um objeto SellerService, automaticamente ele recebe uma
+        //Instância do VendasWebMVCContext também
+        public SellerService(VendasWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        //Método para Listar todos Vendedores
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
