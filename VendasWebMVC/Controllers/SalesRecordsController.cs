1bcabbd234fdd82c83736b86e0453130d34ddbbe
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-28 08:50:50 -0300

    Imlementado Busca simples, View / Controllers / Startup SalesRecords

--- VendasWebMVC/Controllers/SalesRecordsController.cs
+++ VendasWebMVC/Controllers/SalesRecordsController.cs
@@ -3,23 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VendasWebMVC.Services;
 
 namespace VendasWebMVC.Controllers
 {
     public class SalesRecordsController : Controller
     {
+        //Decladando dependência de SalesRecordService
+        private readonly SalesRecordService _salesRecordService;
+
+        //Definindo Contrutor para injetar Dependência
+        public SalesRecordsController(SalesRecordService salesRecordService)
+        {
+            _salesRecordService = salesRecordService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         //Método Busca Simples
-        public IActionResult SimpleSearch()
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (! minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (! maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _salesRecordService.FindByDateAsync(minDate, maxDate);
+            return View(result);
         }
 
-        //
+        //Método Busca Agrupada
         public IActionResult GroupingSearch()
         {
             return View();
---
+++ VendasWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VendasWebMVC.Data;
+using VendasWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace VendasWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        //Declarando Dependência, com readonly para que não possa ser alterada
+        private readonly VendasWebMVCContext _context;
+
+        //Toda vez que for instânciado um objeto SalesRecordService, automaticamente ele recebe uma
+        //Instância do VendasWebMVCContext também
+        public SalesRecordService(VendasWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        //Método para Listar SalesRecord por Período
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+
+            return await result
+                        .Include(x => x.Seller)
+                        .Include(x => x.Seller.Department)
+                        .OrderByDescending(x => x.Date)
+                        .ToListAsync();
+        }
+
+    }
+}
