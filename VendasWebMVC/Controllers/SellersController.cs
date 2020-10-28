1cff05466b502d8696692667fd8c653a7ff5b0ec
Ewerton Maik <ewertonmsa@hotmail.com>
2020-10-27 21:15:34 -0300

    Adicionado Validação na VIEW Seller CREATE e EDIT, Models Seller e Sellercontroller

--- VendasWebMVC/Controllers/SellersController.cs
+++ VendasWebMVC/Controllers/SellersController.cs
@@ -42,6 +42,12 @@ namespace VendasWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if( ! ModelState.IsValid) //Se a requisição recebida não for válida
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -105,6 +111,13 @@ namespace VendasWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid) //Se a requisição recebida não for válida
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id não Corresponde!" });
--- VendasWebMVC/Models/Seller.cs
+++ VendasWebMVC/Models/Seller.cs
@@ -10,16 +10,28 @@ namespace VendasWebMVC.Models
     {
         //Atributos AutoProperties
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Obrigatório!")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Tamanho do {0} deve ser Entre {2} e {1}!")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Digite um E-mail Válido!")]
+        [Required(ErrorMessage = "{0} Obrigatório!")]
         public string Email { get; set; }
+
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} Obrigatório!")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} Obrigatório!")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve ser de {1} até no máximo {2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
